b647180b9295eb75441afa43363629058d9dc865
Chamara Janith <chamaragithub21@gmail.com>
2026-07-07 23:01:19 +0530

    Update footer logos and copyright year in Footer component

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -11,29 +11,39 @@ const MSLogs = [
     height: 120,
   },
   {
+    src: '/images/2026-logo/logo-main-2026.png',
+    alt: 'MS Club Of SLIIT',
+    link: 'https://msclubsliit.org',
+    width: 90,
+    height: 135,
+  }
+];
+
+const FCSCLogo = [{
+  src: '/images/logos/FCSCLogo.png',
+  alt: 'FCSC LOGO',
+  link: 'https://www.facebook.com/sliit.fcsc',
+  width: 80,
+  height: 120,
+},
+{
     src: '/images/logos/MLSALogo.png',
     alt: 'MLSA Logo',
     link: 'https://studentambassadors.microsoft.com',
     width: 50,
     height: 50,
   },
-];
+]
 
-const FCSCLogo = {
-  src: '/images/logos/FCSCLogo.png',
-  alt: 'FCSC LOGO',
-  link: 'https://www.facebook.com/sliit.fcsc',
-  width: 80,
-  height: 120,
-};
+
 
 export default function Footer() {
   return (
-    <footer className='bg-white py-8 shadow-md'>
+    <footer className='py-8 bg-white shadow-md'>
       <hr className='my-6 border-gray-300' />
-      <div className='container mx-auto px-4 lg:px-24 flex flex-col lg:flex-row sm:gap-5 justify-between items-center'>
+      <div className='container flex flex-col items-center justify-between px-4 mx-auto lg:px-24 lg:flex-row sm:gap-50'>
         {/* MS Logos */}
-        <div className='flex items-center space-x-4 sm:space-x-6 mb-4 sm:mb-0 lg:ml-16'>
+        <div className='flex items-center mb-4 space-x-4 sm:space-x-6 sm:mb-0 lg:ml-16'>
           {MSLogs.map((logo, index) => (
             <Link href={logo.link} key={index}>
               <Image
@@ -46,25 +56,29 @@ export default function Footer() {
             </Link>
           ))}
         </div>
+        
         {/* Copyright text */}
-        <div className='text-center text-sm text-gray-700 mb-4 sm:mb-0 mx-10 lg:mx-0'>
-          Copyright &copy; 2025&nbsp;
+        <div className='mx-10 mb-4 text-sm text-center text-gray-700 sm:mb-0 lg:mx-0'>
+          Copyright &copy; 2026&nbsp;
           <a href='https://msclubsliit.org' className='hover:underline'>
             MS Club of SLIIT
           </a>
           . All Rights Reserved.
         </div>
+
         {/* FCSC Logo */}
-        <div className='flex items-center lg:mr-16'>
-          <Link href={FCSCLogo.link}>
-            <Image
-              src={FCSCLogo.src}
-              alt={FCSCLogo.alt}
-              width={FCSCLogo.width}
-              height={FCSCLogo.height}
-              className='cursor-pointer'
-            />
-          </Link>
+        <div className='flex items-center gap-8 lg:mr-16'>
+          {FCSCLogo.map((logo, index) => (
+            <Link href={logo.link} key={index}>
+              <Image
+                src={logo.src}
+                alt={logo.alt}
+                width={logo.width}
+                height={logo.height}
+                className='cursor-pointer'
+              />
+            </Link>
+          ))}
         </div>
       </div>
     </footer>
