ec4e80133c1fe2ee84dfd286bbffdf069885e076
Thamidu-Nadun <nadunrz101@gmail.com>
2026-07-08 23:17:12 +0530

    refactor: update eligibility component according to the UI

--- app/_components/Home/Eligibility/index.jsx
+++ app/_components/Home/Eligibility/index.jsx
@@ -1,37 +1,44 @@
 'use client';
 
-export default function Eligibility() {
+export default function Eligibility () {
   return (
-    <section id="eligibility" className='w-full flex justify-center items-center py-8 sm:py-10 md:py-12 px-2'>
-      <div className='w-full max-w-7xl bg-[#EFEFEF] rounded-[36px] sm:rounded-[48px] md:rounded-[64px] lg:rounded-[72px] px-2 sm:px-6 md:px-10 py-8 sm:py-12 md:py-16 flex flex-col items-center'>
+    <section
+      id="eligibility"
+      className="w-full flex justify-center items-center py-8 sm:py-10 md:py-12 px-5 sm:px-10 md:px-20 bg-gradient-to-r from-red-50 to-[#bbf5fd]"
+    >
+      <div
+        className="w-full max-w-7xl bg-[#EFEFEF] backdrop-filter backdrop-blur-lg bg-opacity-10 rounded-[16px] sm:rounded-[24px] md:rounded-[36px] lg:rounded-[48px] 
+      px-2 sm:px-6 md:px-10 py-8 sm:py-12 md:py-16 flex flex-col items-center"
+        style={{boxShadow: '0px 0px 18px 8px #0000001A'}}
+      >
         <h2
-          className='text-2xl xs:text-3xl sm:text-4xl md:text-5xl font-extrabold text-[#232B36] text-center mb-4 sm:mb-6 tracking-tight'
-          style={{ fontFamily: 'inherit' }}
+          className="text-2xl xs:text-3xl sm:text-4xl md:text-5xl font-extrabold text-black text-center mb-4 sm:mb-6 tracking-tight"
+          style={{fontFamily: 'inherit'}}
         >
           Who Can Participate ?
         </h2>
-        <p className='text-base xs:text-lg sm:text-xl md:text-2xl text-[#495366] text-center max-w-2xl sm:max-w-3xl mb-6 sm:mb-10 md:mb-12 font-medium'>
-          Open to Students of Sri Lanka Institute of Information Technology
-          (SLIIT)
-          <br />
-          from the following batches.
+        <p
+          style={{fontFamily: 'inherit'}}
+          className="text-base xs:text-lg sm:text-xl md:text-2xl text-black font-semibold text-center max-w-2xl sm:max-w-4xl mb-6 sm:mb-10 md:mb-12"
+        >
+          Open to Students of Sri Lanka Institute of Information Technology (SLIIT) from the following batches.
         </p>
-        <div className='flex flex-col sm:flex-row flex-wrap gap-4 sm:gap-6 md:gap-8 w-full justify-center items-center'>
+        <div className="flex flex-col sm:flex-row flex-wrap gap-4 sm:gap-6 md:gap-8 w-full justify-center items-center">
           <div
-            className='bg-[#162B36] text-white text-base xs:text-lg sm:text-xl font-extrabold px-4 xs:px-6 sm:px-8 py-3 sm:py-4 rounded-2xl min-w-[220px] xs:min-w-[260px] sm:min-w-[280px] md:min-w-[320px] text-center'
-            style={{ fontFamily: 'inherit' }}
+            className="bg-[#2E47FF] text-white text-base xs:text-sm sm:text-lg md:text-xl font-extrabold px-4 xs:px-6 sm:px-8 py-3 sm:py-4 rounded-2xl min-w-[220px] xs:min-w-[260px] sm:min-w-[280px] md:min-w-[320px] text-center hover:cursor-pointer"
+            style={{fontFamily: 'inherit'}}
           >
-            All 1st Year Students
+            All First Year Students
           </div>
           <div
-            className='bg-[#162B36] text-white text-base xs:text-lg sm:text-xl font-extrabold px-4 xs:px-6 sm:px-8 py-3 sm:py-4 rounded-2xl min-w-[220px] xs:min-w-[260px] sm:min-w-[280px] md:min-w-[320px] text-center'
-            style={{ fontFamily: 'inherit' }}
+            className="bg-[#2E47FF] text-white text-base xs:text-sm sm:text-lg md:text-xl font-extrabold px-4 xs:px-6 sm:px-8 py-3 sm:py-4 rounded-2xl min-w-[220px] xs:min-w-[260px] sm:min-w-[280px] md:min-w-[320px] text-center hover:cursor-pointer"
+            style={{fontFamily: 'inherit'}}
           >
-            All 2nd Year Students
+            All Second Year Students
           </div>
           <div
-            className='bg-[#162B36] text-white text-base xs:text-lg sm:text-xl font-extrabold px-4 xs:px-6 sm:px-8 py-3 sm:py-4 rounded-2xl min-w-[240px] xs:min-w-[300px] sm:min-w-[320px] md:min-w-[370px] text-center'
-            style={{ fontFamily: 'inherit' }}
+            className="bg-[#2E47FF] text-white text-base xs:text-sm sm:text-lg md:text-xl font-extrabold px-4 xs:px-6 sm:px-8 py-3 sm:py-4 rounded-2xl min-w-[240px] xs:min-w-[300px] sm:min-w-[320px] md:min-w-[370px] text-center hover:cursor-pointer"
+            style={{fontFamily: 'inherit'}}
           >
             All 3rd Year 1st Semester Students
           </div>
