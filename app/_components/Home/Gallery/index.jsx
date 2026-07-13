e10a933d269a4c94d2f291bfd4f32233b9811d0c
Decipher369 <karthigesonshagash2005@gmail.com>
2026-07-13 09:48:17 +0530

    blue colour chnage

--- app/_components/Home/Gallery/index.jsx
+++ app/_components/Home/Gallery/index.jsx
@@ -49,7 +49,7 @@ export default function Gallery() {
           className="inline-flex items-stretch gap-[0.6em] h-[6em]"
           style={{ fontSize: "clamp(6.7px, 0.365vw + 5.33px, 10px)" }}
         >
-          <div className="flex items-center bg-[#3B3EF2] px-[3em] overflow-hidden">
+          <div className="flex items-center bg-[#2E47FF] px-[3em] overflow-hidden">
             <span
               className="text-white text-[5.2em] leading-none translate-y-[2px]"
               style={{
@@ -62,11 +62,11 @@ export default function Gallery() {
             </span>
           </div>
           <div className="flex items-stretch" aria-hidden="true">
-            <i className="block w-[1.8em] mr-[0.4em] bg-[#3B3EF2]" />
-            <i className="block w-[1.1em] mr-[0.4em] bg-[#3B3EF2]" />
-            <i className="block w-[1em] mr-[0.3em] bg-[#3B3EF2]" />
-            <i className="block w-[0.5em] mr-[0.3em] bg-[#3B3EF2]" />
-            <i className="block w-[0.1em] min-w-[1px] bg-[#3B3EF2]" />
+            <i className="block w-[1.8em] mr-[0.4em] bg-[#2E47FF]" />
+            <i className="block w-[1.1em] mr-[0.4em] bg-[#2E47FF]" />
+            <i className="block w-[1em] mr-[0.3em] bg-[#2E47FF]" />
+            <i className="block w-[0.5em] mr-[0.3em] bg-[#2E47FF]" />
+            <i className="block w-[0.1em] min-w-[1px] bg-[#2E47FF]" />
           </div>
         </div>
       </div>
